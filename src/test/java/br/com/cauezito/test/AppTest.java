6767336e4dd3482a8d68ea5ab7f8fba5395f2ad7
Cauê Santos <caah.sfc@live.com>
2020-07-16 13:58:48 -0300

    Inserindo usuário

--- src/test/java/br/com/cauezito/test/AppTest.java
+++ src/test/java/br/com/cauezito/test/AppTest.java
@@ -7,17 +7,30 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import br.com.cauezito.spring.dao.InterfaceSpringDataUsuario;
+import br.com.cauezito.spring.model.Usuario;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"classpath:META-INF/spring-config.xml"})
 public class AppTest {
 
 	@Autowired
-	private InterfaceSpringDataUsuario isdu;
+	private InterfaceSpringDataUsuario springDataUsuario;
 	
 	@Test
-	public void insert() {
-		System.out.println("Iniciou");
+	public void criarTabela() {
+		System.out.println("criando tabela...");
 	}
+	
+	@Test
+	public void inserir() {
+		Usuario usuario = new Usuario();
+		usuario.setIdade(15);
+		usuario.setLogin("cauezito");
+		usuario.setSenha("123@987");
+		usuario.setNome("Cauê");
+		
+		springDataUsuario.save(usuario);
+	}
+
 
 }
